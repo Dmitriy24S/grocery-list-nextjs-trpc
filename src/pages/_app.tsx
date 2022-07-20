208c03aefdf136b637557bc0e5c93037d24203f1
Dmitriy <devilins24@gmail.com>
2022-07-20 12:05:22 +0300

    refactor: :construction: try process.env.NEXT_PUBLIC_VERCEL_URL instead of VERCEL_URL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,11 +9,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 // export default MyApp
+//  ? `https://${process.env.VERCEL_URL}/api/trpc`
+// : "http://localhost:3002/api/trpc";
 export default withTRPC<ServerRouter>({
   config({ ctx }) {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
+
     return { url };
   },
   ssr: true,
